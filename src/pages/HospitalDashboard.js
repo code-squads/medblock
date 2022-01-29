f2ad92baf1b7c9829838730b3ac136e0f3fe5703
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2022-01-30 02:41:49 +0530

    Hospital Dashboard (#12)
    Signed-off-by: Aniruddha Shriwant <aniruddhashriwant@gmail.com>

--- src/pages/HospitalDashboard.js
+++ src/pages/HospitalDashboard.js
@@ -1,7 +1,229 @@
-const HospitalDashboard = () => {
-    return (
-        <div>Hospital Dashboard</div>
-    )
-}
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { QRCode } from "react-qrcode-logo";
+import Nbsp from "../utils/nbsp";
 
-export default HospitalDashboard
+import { AUTHORITY_TYPES } from "../Constants/authorityTypes";
+import { useAuth } from "../services/authorization";
+import { getInitials } from "../utils/dataUtils";
+
+import {
+  HospitalDashboardContainer,
+  Left,
+  Right,
+  Center,
+  Navlink,
+  NavlinkActive,
+  NavMenuList,
+  ListItems,
+  ListItemsActive,
+  Name,
+  TitleHosp,
+  HospitalStyling,
+  Row1,
+  Row2,
+  Row3,
+  MedicalSVG,
+  DiagnosedSVG,
+  SecuritySVG,
+  GraphSVG,
+  DoctorSVG,
+  HopitalCard,
+  HospitalCardName,
+  Circle,
+  HospCardReg,
+  HospCardAdd,
+  HospCardHeading,
+  HospCardDetails,
+  HospCardRow3,
+  FCName,
+  Note,
+  NotesDiv,
+  NoteTitle,
+  NoteDescription,
+} from "./HospitalDashboard.styled";
+
+import { ReactComponent as Dashboardlogo } from "../assets/icons/admin/dashboardicon.svg";
+import { ReactComponent as LogoutLogo } from "../assets/icons/admin/logouticon.svg";
+import { ReactComponent as UserLogo } from "../assets/icons/admin/usericon.svg";
+import { ReactComponent as SettingsLogo } from "../assets/icons/admin/settingsicon.svg";
+import { ReactComponent as DeclineLogo } from "../assets/icons/hospital/Decline.svg";
+
+import { ReactComponent as HospitalLogo } from "../assets/icons/hospital/hospital.svg";
+import { ReactComponent as TickMark } from "../assets/icons/admin/greentick.svg";
+
+import { ReactComponent as MedStaff } from "../assets/icons/hospital/MedStaff.svg";
+import { ReactComponent as Diagnosed } from "../assets/icons/hospital/diagnosed.svg";
+import { ReactComponent as Security } from "../assets/icons/admin/security.svg";
+import { ReactComponent as Graph } from "../assets/icons/hospital/graph.svg";
+import { ReactComponent as Doctor } from "../assets/icons/admin/doctor.svg";
+
+const HospitalDashboard = () => {
+  const auth = useAuth();
+  const hospitalInfo = auth.entityInfo;
+
+  if (!auth.loggedIn || !auth.entityInfo || !auth.wallet || !auth.authority) {
+    auth.logout();
+    return <Redirect to="/login/hospital" />;
+  }
+
+  if (auth.authority !== AUTHORITY_TYPES.HOSPITAL) return <Redirect to="/" />;
+
+  return (
+    <HospitalDashboardContainer>
+      <Left>
+        <NavMenuList>
+          <ListItemsActive>
+            {/* <Navlink to="/newRecord">Dashboard</Navlink> */}
+            <Navlink to="/hospitalDashboard">
+              <div>
+                <Dashboardlogo />
+              </div>
+              <div>
+                <Nbsp />
+                Dashboard
+              </div>
+            </Navlink>
+          </ListItemsActive>
+          <ListItems>
+            <NavlinkActive to="/newRecord">
+              <div>
+                <UserLogo />
+              </div>
+              <div>
+                <Nbsp />
+                Add New Record
+              </div>
+            </NavlinkActive>
+          </ListItems>
+          <ListItems>
+            <Navlink to="/declinedRecords">
+              <div>
+                <DeclineLogo
+                  style={{ width: "15px", height: "15px", marginRight: "5px" }}
+                />
+              </div>
+              <div>
+                <Nbsp />
+                Declined Records
+              </div>
+            </Navlink>
+          </ListItems>
+          <ListItems>
+            <Navlink onClick={auth.logout} to="">
+              <div>
+                <LogoutLogo />
+              </div>
+              <div>
+                {" "}
+                <Nbsp />
+                Logout
+              </div>
+            </Navlink>
+          </ListItems>
+          <ListItems>
+            <Navlink to="#">
+              <div>
+                <SettingsLogo />
+              </div>
+              <div>
+                <Nbsp />
+                Settings
+              </div>
+            </Navlink>
+          </ListItems>
+        </NavMenuList>
+        <Note>
+          <NotesDiv>
+            <NoteTitle>
+              Note
+              <Nbsp />:
+              <Nbsp />
+            </NoteTitle>
+            <NoteDescription>
+              Admin must strictly verify the revelant doucments during the
+              registration process
+            </NoteDescription>
+          </NotesDiv>
+        </Note>
+      </Left>
+      <Center>
+        <Row1>
+          <MedicalSVG>
+            <MedStaff />
+          </MedicalSVG>
+          <DiagnosedSVG>
+            <Diagnosed />
+          </DiagnosedSVG>
+        </Row1>
+        <Row2>
+          <SecuritySVG>
+            <Security />
+          </SecuritySVG>
+        </Row2>
+        <Row3>
+          <GraphSVG>
+            <Graph />
+          </GraphSVG>
+          <DoctorSVG>
+            <Doctor />
+          </DoctorSVG>
+        </Row3>
+      </Center>
+      <Right>
+        <div>
+          <center>
+            <HospitalStyling>
+              <HospitalLogo />
+            </HospitalStyling>
+          </center>
+        </div>
+        <div>
+          <center>
+            <Name>{hospitalInfo.name}</Name>
+            <br />
+            <TitleHosp>
+              Hospital <TickMark />
+            </TitleHosp>
+            <br />
+            <QRCode size={100} value={auth.wallet.address} fgColor="#101010" />
+          </center>
+        </div>
+        <center>
+          <HopitalCard>
+            <HospCardHeading>
+              <Circle>{getInitials(hospitalInfo.name)}</Circle>
+              <HospitalCardName>{hospitalInfo.name}</HospitalCardName>
+            </HospCardHeading>
+            <HospCardDetails>
+              <HospCardReg>
+                <div>
+                  Reg No.
+                  <Nbsp />:
+                  <Nbsp />
+                </div>
+                <div>
+                  <Nbsp />
+                  {hospitalInfo.govtLicenseNumber}
+                </div>
+              </HospCardReg>
+              <HospCardAdd>
+                <div>
+                  Address
+                  <Nbsp />:
+                  <Nbsp />
+                </div>
+                <div>{hospitalInfo.Address}</div>
+              </HospCardAdd>
+            </HospCardDetails>
+            <HospCardRow3>
+              <FCName>FC Card</FCName>
+            </HospCardRow3>
+          </HopitalCard>
+        </center>
+      </Right>
+    </HospitalDashboardContainer>
+  );
+};
+
+export default HospitalDashboard;
---
+++ src/pages/HospitalDashboard.styled.js
@@ -0,0 +1,230 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const HospitalDashboardContainer = styled.div`
+  width: 100%;
+  /* min-height: 100vh; */
+  height: calc(100vh - 56px);
+  display: flex;
+  flex-direction: row;
+  color: #404040;
+  font-family: "Inter";
+`;
+
+// Left Div :
+
+export const Left = styled.div`
+  width: 18%;
+  min-height: 100%;
+  /* background-color: cyan; */
+  border-right: 2px solid #d1d5d8;
+`;
+
+export const Navlink = styled(Link)`
+  color: #404040;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  margin-left: 1rem;
+`;
+
+export const NavlinkActive = styled(Link)`
+  color: #404040;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  margin-left: 0.9rem;
+`;
+
+export const NavMenuList = styled.ul`
+  font-size: 16px;
+  font-weight: 400;
+  margin-top: 20px;
+  list-style: none;
+  padding-left: 0rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ListItems = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 0.25rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+`;
+
+export const ListItemsActive = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 0.25rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  border-left: 4px solid #387ed1;
+  border-radius: 2px;
+  margin-left: 0.1rem;
+`;
+
+export const Note = styled.div`
+  padding-left: 0.5rem;
+  margin-top: 4rem;
+`;
+
+export const NotesDiv = styled.div`
+  line-height: 12px;
+  display: flex;
+  border-left: 2px solid #c93636;
+  padding-left: 0.2rem;
+`;
+
+export const NoteTitle = styled.text`
+  color: #c93636;
+  font-size: 10px;
+`;
+export const NoteDescription = styled.text`
+  font-size: 10px;
+`;
+
+// Center Div :
+
+export const Center = styled.div`
+  position: relative;
+  width: 62%;
+  min-height: 100%;
+  border-right: 2px solid #d1d5d8;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Row1 = styled.div`
+  margin-top: 2rem;
+`;
+
+export const Row2 = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-top: 0.5rem;
+  padding-bottom: 1rem;
+`;
+
+export const Row3 = styled.div``;
+
+export const MedicalSVG = styled.span`
+  margin-left: 2rem;
+  margin-right: 2rem;
+`;
+
+export const DiagnosedSVG = styled.span``;
+
+export const SecuritySVG = styled.span`
+  margin-top: 1rem;
+  margin-right: 2rem;
+`;
+
+export const GraphSVG = styled.span`
+  margin-left: 2rem;
+  margin-right: 2rem;
+`;
+
+export const DoctorSVG = styled.span`
+  padding-left: 2rem;
+`;
+
+// Right Div :
+
+export const Right = styled.div`
+  width: 25%;
+  min-height: 100%;
+  align-content: center;
+  /* background-color: cyan; */
+`;
+
+export const HospitalStyling = styled.div`
+  margin-top: 50px;
+  padding-bottom: 10px;
+`;
+
+export const Name = styled.text`
+  color: #202020;
+  font-weight: 400;
+  font-size: 18px;
+`;
+
+export const TitleHosp = styled.text`
+  padding-top: 8px;
+  color: #404040;
+  font-size: 15px;
+  padding-bottom: 10px;
+`;
+
+export const HopitalCard = styled.div`
+  width: 17rem;
+  margin-top: 2rem;
+  margin-left: 2rem;
+  margin-right: 1rem;
+  box-shadow: 0px 1px 5px 0px #787878;
+  /* display: flex; */
+  /* justify-content: center; */
+`;
+
+export const HospCardHeading = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const HospitalCardName = styled.text`
+  color: #202020;
+  font-weight: 400;
+  font-size: 16px;
+  margin-top: 8px;
+  margin-left: 5px;
+`;
+
+export const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 32px;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: 5px;
+  border-radius: 50%;
+  background-color: #151e28;
+  color: white;
+`;
+
+export const HospCardDetails = styled.div`
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  margin-left: 0.5rem;
+  margin-top: 0.5rem;
+`;
+
+export const HospCardReg = styled.text`
+  color: #202020;
+  font-weight: 400;
+  font-size: 10px;
+  display: flex;
+`;
+
+export const HospCardAdd = styled.text`
+  color: #202020;
+  font-weight: 400;
+  font-size: 10px;
+  display: flex;
+`;
+
+export const HospCardRow3 = styled.div`
+  width: 100%;
+  align-items: flex-end;
+  text-align: right;
+  padding: 15px;
+`;
+
+export const FCName = styled.text`
+  color: #505050;
+  font-weight: 400;
+  font-size: 14px;
+`;
