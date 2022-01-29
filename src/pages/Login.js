26d3f472650a323cebe945d40b5cc03ed8135f33
Rohan Darade <rohandarade29@gmail.com>
2022-01-29 18:26:53 +0530

    Login UI
    Signed-off-by: Aniruddha Shriwant <aniruddhashriwant@gmail.com>

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,138 @@
-const Login = () => {
-    return (
-        <div>Login Page</div>
-    )
-}
+import React, { useState } from "react";
+import { useParams, Redirect } from "react-router-dom";
+import { TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import QrReader from "react-qr-reader";
 
-export default Login
+import basicInfo from "../assets/illustrations/basicInfo2.png";
+import CoreButton from "../components/core/Button";
+import {
+  AppNameContainer,
+  AppOveriewIllustration,
+  ButtonContainer,
+  Container,
+  CreateAccountText,
+  Heading,
+  OrDiv,
+  PageName,
+  ScanOrButton,
+  ScanOrButtonMobile,
+  SubContainer1,
+  SubContainer2,
+  SubHeading,
+  TextFieldContainer,
+} from "./Login.styled";
+
+const PatientLogin2 = () => {
+  const [pk, setPK] = useState("");
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const [openWebcam, setOpenWebcam] = useState(false);
+  const [openWebcamMobile, setOpenWebcamMobile] = useState(false);
+
+  const { type } = useParams();
+  function login() {
+    setIsLoggingIn(true);
+  }
+
+  const onHandlerErrWebcam = (error) => {
+    console.log(error);
+  };
+
+  const onHandlerResultWebcam = (result) => {
+    if (result) {
+      setPK(result);
+      setOpenWebcam(false);
+      setOpenWebcamMobile(false);
+    }
+  };
+
+  return (
+    <Container>
+      <SubContainer1>
+        <AppNameContainer>
+          <img src={basicInfo} alt="Mediblock" />
+          <Heading>Decentralized platform</Heading>
+          <SubHeading>to store your medical history</SubHeading>
+        </AppNameContainer>
+        {!openWebcam && <AppOveriewIllustration />}
+        {openWebcam && (
+          <QrReader
+            style={{
+              width: "50%",
+              marginTop: "40px",
+              border: "solid black 1px",
+            }}
+            delay={300}
+            onError={onHandlerErrWebcam}
+            onScan={onHandlerResultWebcam}
+          />
+        )}
+      </SubContainer1>
+      <SubContainer2>
+        <PageName>
+          {type === "patient"
+            ? "Patient"
+            : type === "hospital"
+            ? "Hospital"
+            : "Admin"}{" "}
+          Login
+        </PageName>
+        {openWebcamMobile && (
+          <QrReader
+            style={{
+              width: "60%",
+              marginTop: "40px",
+              border: "solid black 1px",
+            }}
+            delay={300}
+            onError={onHandlerErrWebcam}
+            onScan={onHandlerResultWebcam}
+          />
+        )}
+        {!openWebcamMobile && (
+          <TextFieldContainer>
+            <TextField
+              label="Private Key"
+              required
+              fullWidth
+              autoComplete="nope"
+              value={pk}
+              onChange={(e) => setPK(e.target.value)}
+            />
+          </TextFieldContainer>
+        )}
+        {!openWebcamMobile && <OrDiv>Or</OrDiv>}
+        <ButtonContainer>
+          <ScanOrButton onClick={() => setOpenWebcam(!openWebcam)}>
+            {openWebcam ? "Close Camera" : "Scan QR"}
+          </ScanOrButton>
+          <ScanOrButtonMobile
+            onClick={() => setOpenWebcamMobile(!openWebcamMobile)}
+          >
+            {openWebcamMobile ? "Close Camera" : "Scan QR"}
+          </ScanOrButtonMobile>
+        </ButtonContainer>
+        <CoreButton
+          disabled={isLoggingIn}
+          style={{ marginTop: "80px" }}
+          onClick={!isLoggingIn ? login : null}
+        >
+          {isLoggingIn ? (
+            <>
+              Trying to login. &nbsp;&nbsp;
+              <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
+            </>
+          ) : (
+            "Login"
+          )}
+        </CoreButton>
+        <CreateAccountText>
+          New user? visit nearest center to register yourself
+        </CreateAccountText>
+      </SubContainer2>
+    </Container>
+  );
+};
+
+export default PatientLogin2;
---
+++ src/pages/Login.styled.js
@@ -0,0 +1,121 @@
+import styled from "styled-components";
+import AppOverview from "../assets/illustrations/appOverview.png";
+import { CoreButton } from "../components/core";
+
+export const Container = styled.div`
+  height: calc(100vh - 56px);
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+export const SubContainer1 = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+export const AppNameContainer = styled.div`
+  position: relative;
+  font-family: "Inter";
+  margin-top: 50px;
+`;
+export const Heading = styled.span`
+  position: absolute;
+  margin: auto;
+  top: 10px;
+  left: 15px;
+  right: 0;
+  text-align: center;
+  font-size: 28px;
+  font-weight: 400px;
+  color: #404040;
+`;
+export const SubHeading = styled.span`
+  position: absolute;
+  top: 45px;
+  left: 15px;
+  right: 0;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 400;
+  color: #606060;
+`;
+export const AppOveriewIllustration = styled.img.attrs({
+  src: `${AppOverview}`,
+})`
+  margin-top: auto;
+  margin-bottom: 65px;
+`;
+export const SubContainer2 = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Inter";
+  @media (max-width: 1000px) {
+    width: 100%;
+  }
+`;
+export const PageName = styled.div`
+  font-size: 28px;
+  font-weight: 400;
+  color: #404040;
+  margin-top: 70px;
+`;
+export const TextFieldContainer = styled.div`
+  width: 70%;
+  max-width: 500px;
+  margin-top: 100px;
+  @media (max-width: 600px) {
+    width: 85%;
+  }
+`;
+export const OrDiv = styled.div`
+  margin-top: 30px;
+`;
+export const ButtonContainer = styled.div`
+  width: 70%;
+  max-width: 500px;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    justify-content: space-between;
+    row-gap: 30px;
+    width: 80%;
+  }
+`;
+
+export const ScanOrButton = styled(CoreButton)`
+  color: #202020;
+  background-color: #ffde00;
+  font-size: 16px;
+  width: 45%;
+  @media (max-width: 1000px) {
+    /* width: 100%; */
+    display: none;
+  }
+`;
+export const ScanOrButtonMobile = styled(CoreButton)`
+  color: #202020;
+  background-color: #ffde00;
+  font-size: 16px;
+  width: 45%;
+  @media (min-width: 1000px) {
+    display: none;
+  }
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
+export const CreateAccountText = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+  color: #606060;
+  margin-top: 12px;
+`;
